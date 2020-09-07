980ab462094ba1d41f15e19fd815a0f25ec321cc
Rakesh Kumar <rkrakesh133@gmail.com>
2020-09-07 10:10:23 +0530

    A change for branch 2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, "hello from Android", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "THis is branch number 2", Toast.LENGTH_LONG).show()
     }
 }
